14f9075a622ccdd42ba37b0c193bbf3e122dd87a
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-08-27 17:55:32 +0200

    Added bare structure.

--- include/minishell.h
+++ include/minishell.h
@@ -13,4 +13,33 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+#include <curses.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef bool	t_error;
+
+#define ERROR 1
+#define NO_ERROR 0
+
+typedef struct	s_cmd
+{
+	int			in_fd;
+	int 		out_fd;
+	char 		*arg;
+}				t_cmd;
+
 #endif
